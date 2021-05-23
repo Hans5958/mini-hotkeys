b19e7e1cef28196b919da263c4be660f887d14e2
Hans5958 <email5958.1@gmail.com>
2021-05-23 22:34:22 +0700

    Add SikuliX scripts

---
+++ SikuliX/LGDC PBL.sikuli/LGDC PBL.py
@@ -0,0 +1,128 @@
+# --------------------
+# LIME GREEN 
+# DOJO GREEN
+# PAINT BY LETTERS
+# SEMI-BOT
+# --------------------
+
+# Start by opening the book. Run the bot on the cover.
+
+# START
+click("1493628605609.png");click("1493628622645.png")
+
+# PAGE 1
+type("I am the busiest penguin I know.")
+sleep(0.5)
+type("Cleaning the windows at the Dojo.")
+sleep(0.5)
+type("Wiping them down and getting them shined.")
+sleep(0.5)
+type("Making sure no one has left streaks behind.")
+sleep(5)
+dragDrop(Pattern("1493630460630.png").targetOffset(-5,-27), "1493630473077.png");sleep(5.5);dragDrop("1493628986816-1.png", Pattern("1493630528146.png").targetOffset(33,-34));dragDrop(Pattern("1493629131925-1.png").targetOffset(44,75),Pattern("1493629131925-1.png").targetOffset(44,75))
+click(Pattern("1493629669430.png").targetOffset(-26,-54))
+
+# PAGE 2
+type("Then one day while I was working away,")
+sleep(0.5)
+type("Wouldn't you know it, some penguins they came")
+sleep(0.5)
+type("and brought this container as big as a tree.")
+sleep(0.5)     
+type("and filled to the top with glowing lime green!")
+sleep(0.5)    
+dragDrop(Pattern("1493630622013.png").similar(0.80), "1493630628041.png");dragDrop(Pattern("1493629787162-1.png").targetOffset(66,86),Pattern("1493629787162-1.png").targetOffset(66,86) )
+click(Pattern("1493629669430-1.png").targetOffset(-26,-54))
+
+# PAGE 3
+type("I gazed in amazement as they fell in.")
+sleep(0.5)
+type("Then one of them spoke to me with a grin,")
+sleep(0.5)
+type("It's a colorful party for all to express!")
+sleep(0.5)
+type("But while he was so happy, the place was a mess!")
+sleep(5)
+hover("1493630736904-1.png");mouseDown(Button.LEFT);mouseMove(Pattern("1493630783268-1.png").targetOffset(17,48));mouseMove(Pattern("1493630783268-1.png").targetOffset(338,103));mouseMove(Pattern("1493630783268-1.png").targetOffset(17,48));mouseMove(Pattern("1493630783268-1.png").targetOffset(338,103));mouseMove(Pattern("1493630783268-1.png").targetOffset(17,48));mouseMove(Pattern("1493630783268-1.png").targetOffset(338,103));mouseMove(Pattern("1493630783268-1.png").targetOffset(17,48));mouseMove(Pattern("1493630783268-1.png").targetOffset(338,103));mouseMove(Pattern("1493630783268-1.png").targetOffset(17,48));mouseMove(Pattern("1493630783268-1.png").targetOffset(338,103));mouseMove(Pattern("1493630783268-1.png").targetOffset(17,48));mouseMove(Pattern("1493630783268-1.png").targetOffset(338,103));mouseMove(Pattern("1493630783268-1.png").targetOffset(17,48));mouseMove(Pattern("1493630783268-1.png").targetOffset(338,103));mouseMove(Pattern("1493630783268-1.png").targetOffset(17,48));mouseMove(Pattern("1493630783268-1.png").targetOffset(338,103));mouseMove(Pattern("1493630783268-1.png").targetOffset(17,48));mouseMove(Pattern("1493630783268-1.png").targetOffset(338,103));mouseMove(Pattern("1493630783268-1.png").targetOffset(17,48));mouseMove(Pattern("1493630783268-1.png").targetOffset(338,103));mouseMove(Pattern("1493630783268-1.png").targetOffset(17,48));mouseMove(Pattern("1493630783268-1.png").targetOffset(338,103));mouseMove(Pattern("1493630783268-1.png").targetOffset(17,48));mouseMove(Pattern("1493630783268-1.png").targetOffset(338,103));mouseMove(Pattern("1493630783268-1.png").targetOffset(17,48));mouseMove(Pattern("1493630783268-1.png").targetOffset(338,103));mouseMove(Pattern("1493630783268-1.png").targetOffset(17,48));mouseMove(Pattern("1493630783268-1.png").targetOffset(338,103));mouseMove(Pattern("1493630783268-1.png").targetOffset(17,48));mouseMove(Pattern("1493630783268-1.png").targetOffset(338,103));mouseMove(Pattern("1493630783268-1.png").targetOffset(17,48));mouseMove(Pattern("1493630783268-1.png").targetOffset(338,103));mouseMove(Pattern("1493630783268-1.png").targetOffset(17,48));mouseMove(Pattern("1493630783268-1.png").targetOffset(338,103));mouseMove(Pattern("1493630783268-1.png").targetOffset(17,48));mouseMove(Pattern("1493630783268-1.png").targetOffset(338,103));mouseMove(Pattern("1493630783268-1.png").targetOffset(17,48));mouseMove(Pattern("1493630783268-1.png").targetOffset(338,103));mouseMove(Pattern("1493630783268-1.png").targetOffset(17,48));mouseMove(Pattern("1493630783268-1.png").targetOffset(338,103));mouseMove(Pattern("1493630783268-1.png").targetOffset(17,48));mouseMove(Pattern("1493630783268-1.png").targetOffset(338,103));mouseMove(Pattern("1493630783268-1.png").targetOffset(17,48));mouseMove(Pattern("1493630783268-1.png").targetOffset(338,103));mouseMove(Pattern("1493630783268-1.png").targetOffset(17,48));mouseMove(Pattern("1493630783268-1.png").targetOffset(338,103));mouseUp(Button.LEFT);popup("Please confirm that the coin is collected.\nOtherwise, keep \"scratching\" until you get your coin.")
+click(Pattern("1493629669430-3.png").targetOffset(-26,-54))
+
+# PAGE 4
+type("There was paint on the walls and paint on the ceiling.")
+sleep(0.5)
+type("This paint-splattered place was so unappealing!")
+sleep(0.5)
+type("And just when I thought I could take it no more,")
+sleep(0.5)
+type("a penguin began to make tracks on the floor!")
+popup("Please done this hidden coin part for yourself.\n\nPress OK if you have collected the coin.")
+click(Pattern("1493629669430-3.png").targetOffset(-26,-54))
+
+# PAGE 5
+type("\"I just cleaned the floor!\" I loudly proclaimed,")
+sleep(0.5)
+type("\"And now I will have to go mop it again!\"")
+sleep(0.5)
+type("But wouldn't you know it, this mop that I used")
+sleep(0.5)
+type("Was dropped in the bucket of lime-colored ooze!")
+sleep(1)
+hover("1493638926920-1.png");hover(Pattern("1493632616507-4.png").targetOffset(-26,0));sleep(6);click(Pattern("1493632767731-4.png").targetOffset(39,100));click(Pattern("1493632767731-4.png").targetOffset(195,104));click(Pattern("1493632767731-4.png").targetOffset(280,90));click(Pattern("1493632767731-4.png").targetOffset(123,174));sleep(1);dragDrop(Pattern("1493632966746-4.png").targetOffset(36,113), Pattern("1493632966746-4.png").targetOffset(36,113))
+click(Pattern("1493629669430-11.png").targetOffset(-26,-54))
+
+# PAGE 6
+type("Now I faced a decision of just what to do.")
+sleep(0.5)
+type("My mop was unusable thanks to this crew")
+sleep(0.5)
+type("The rags that I used were just far too worn out.")
+sleep(0.5)
+type("So I stood on that paint can and started to shout.")
+sleep(1)
+
+dragDrop(Pattern("1493633821921-4.png").similar(0.51), Pattern("1493633827941-4.png").targetOffset(205,-32));dragDrop(Pattern("1493633859770-4.png").targetOffset(87,-217), Pattern("1493633859770-4.png").targetOffset(87,-217))
+click(Pattern("1493629669430-10.png").targetOffset(-26,-54))
+
+# PAGE 7
+type("Then a penguin responded, \"You seem so upset!")
+sleep(0.5)
+type("It's time for a party, but did you forget?")
+sleep(0.5)
+type("Perhaps there's a way for me to help you.")
+sleep(0.5)
+type("And I think that I know just what we should do!")
+sleep(1)
+click("1493634648108-2.png");sleep(5);hover(Pattern("1493634687646-2.png").targetOffset(189,-48));mouseDown(Button.LEFT);mouseMove("1493634775891-2.png");mouseMove("1493634813571-2.png");mouseMove("1493634838317-2.png");mouseMove("1493634813571-2.png");mouseMove(Pattern("1493634813571-2.png").targetOffset(-5,184));mouseMove(Pattern("1493634813571-2.png").targetOffset(-227,181));mouseMove("1493634981256-2.png");mouseMove(Pattern("1493635045516-2.png").targetOffset(-217,-41));mouseMove(Pattern("1493635045516-2.png").targetOffset(6,3));mouseMove(Pattern("1493635106711-2.png").targetOffset(48,-35));mouseMove("1493635136240-2.png");mouseDown(Button.LEFT);dragDrop(Pattern("1493635207096-2.png").targetOffset(189,72),Pattern("1493635207096-2.png").targetOffset(189,72) )
+click(Pattern("1493629669430-10.png").targetOffset(-26,-54))
+
+# PAGE 8
+type("The penguin she pointed towards a large box.")
+sleep(0.5)
+type("Containing some capes and a couple old socks.")
+sleep(0.5)
+type("\"Let's use up these capes to clean up the mess!\"")
+sleep(0.5)
+type("Now that's an idea I wouldn't have guessed!")
+sleep(1)
+click("1493637440148.png");click("1493637440148.png");click("1493637440148.png");click("1493637440148.png");click("1493637440148.png");dragDrop("1493637440148.png", "1493637508817.png");dragDrop("1493637440148.png", "1493637524882.png");dragDrop(Pattern("1493637801978.png").targetOffset(-4,47), Pattern("1493637801978.png").targetOffset(-4,47))
+click(Pattern("1493629669430-3.png").targetOffset(-26,-54))
+
+# PAGE 9
+type("When the party was over, then everyone joined!")
+sleep(0.5)
+type("They helped clean the Dojo without needing a coin.")
+sleep(0.5)
+type("And when we were done, the place was so perfect.")
+sleep(0.5)
+type("You'd never have guessed it was once a green wreck!")
+sleep(1)
+type("The End")
+sleep(3)
+click(Pattern("1493629669430-3.png").targetOffset(-26,-54))
+
+# END CHECK
+wait("1493639918250.png")
+sleep(1)
+if exists("1493640002174.png"):
+	popup("Operation completed successfully!","Result")
+else:
+	popup("Operation failed!\n\nThis may caused by missed coins.","Result")
